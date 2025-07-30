d8aef0f597acfe7522355270f3b4fc9caf3d5f62
dilip4640 <dilipmullu002@gmail.com>
2025-07-31 00:14:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let link1 = document.getElementById('about-more1');
+
+link1.addEventListener('click', function() {
+    
+    window.open('https://en.wikipedia.org/wiki/Tent', '_blank'); 
+});
+//2nd One
+let link2 = document.getElementById('about-more2');
+
+link2.addEventListener('click', function() {
+    
+    window.open('https://en.wikipedia.org/wiki/Sleeping_bag', '_blank'); 
+});
+
+//3rd one
+let link3 = document.getElementById('about-more3');
+
+link3.addEventListener('click', function() {
+    
+    window.open('https://en.wikipedia.org/wiki/Portable_stove', '_blank'); 
+});
+
+//4th one
+let link4 = document.getElementById('about-more4');
+
+link4.addEventListener('click', function() {
+    
+    window.open('https://en.wikipedia.org/wiki/Travel_pack', '_blank'); 
+});
+//5th one
+let link5 = document.getElementById('about-more5');
+
+link5.addEventListener('click', function() {
+    
+    window.open('https://en.wikipedia.org/wiki/Solar_lamp', '_blank'); 
+});
+//6th one
+let link6 = document.getElementById('about-more6');
+
+link6.addEventListener('click', function() {
+    
+    window.open('https://en.wikipedia.org/wiki/Camping_chair', '_blank'); 
+});
+
+//BUTTON FUNCTION
+let submit=document.getElementById('btnn');
+submit.addEventListener('click',()=>{
+    alert('Successful');
+})
